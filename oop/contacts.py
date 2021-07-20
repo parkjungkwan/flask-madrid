7ed4638a3b1cd03f799bf3411691d726d028d41b
parkjungkwan <pakjkwan@gmail.com>
2021-07-20 11:20:49 +0900

    code Contacts

---
+++ oop/contacts.py
@@ -0,0 +1,35 @@
+"""
+name, phone, email, address
+"""
+
+
+class Contacts(object):
+    def __init__(self):
+        pass
+
+    def to_string(self):
+        print(f'')
+
+
+def set_contact():
+    return None
+
+
+def get_contacts():
+    pass
+
+
+def del_contact(contacts, name):
+    pass
+
+
+def print_menu():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- oop/grade_with_name.py
+++ oop/grade_with_name.py
@@ -1,7 +1,9 @@
-'''
+"""
 학생이름, 국어 kor, 영어 eng, 수학 math 를 입력받아서
 학생이름, 평균, 학점을 출력하시오
-'''
+"""
+
+
 class Grade(object):
 
     def __init__(self, name):
@@ -14,26 +16,29 @@ class Grade(object):
     def avg(self):
         return sum(self.scores) / len(self.scores)
 
-    @staticmethod
-    def main():
-        grade = Grade(input('Input Student Name:'))
-        for i in ['Korean','English','Math']:
-            grade.addScores(int(input(f'{i}: ')))
-        avg = grade.avg()
-        if avg >= 90:
-            result = 'A'
-        elif avg >= 80:
-            result = 'B'
-        elif avg >= 70:
-            result = 'C'
-        elif avg >= 60:
-            result = 'D'
-        elif avg >= 50:
-            result = 'E'
-        else:
-            result = 'F'
-        print(f'{result}')
 
-Grade.main()
+def main():
+    grade = Grade(input('Input Student Name:'))
+    for i in ['Korean','English','Math']:
+        grade.addScores(int(input(f'{i}: ')))
+    avg = grade.avg()
+    if avg >= 90:
+        result = 'A'
+    elif avg >= 80:
+        result = 'B'
+    elif avg >= 70:
+        result = 'C'
+    elif avg >= 60:
+        result = 'D'
+    elif avg >= 50:
+        result = 'E'
+    else:
+        result = 'F'
+    print(f'{result}')
+
+
+if __name__ == '__main__':
+    main()
+
 
 
