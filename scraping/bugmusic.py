95833bfc5a94d3b51c9a17c008f084458f7a25fe
parkjungkwan <pakjkwan@gmail.com>
2021-07-21 12:16:40 +0900

    12

--- scraping/bugmusic.py
+++ scraping/bugmusic.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import urllib.parse
 """
 지원하는 Parser 종류
 "html.parser" : 빠르지만 유연하지 않기 때문에 단순한 HTML문서에 사용합니다.
@@ -16,16 +17,30 @@ class Bugmusic(object):
 
     def scrap(self):
         soup = BeautifulSoup(urlopen(self.url), 'lxml')
-        n_artists = 0
-        n_title = 0
-        ls = soup.find_all(name='p', attrs={'class':'artist'})
-        print(f'List size is {len(ls)}')
-        for i in ls:
-            n_artists += 1
-            print(str(n_artists) + " Rank")
-            print("Artist : " + i.find('a').text)
+        _ = 0
+        artists = soup.find_all(name='p', attrs={'class':'artist'})
+        titles = soup.find_all(name='p', attrs={'class':'title'})
+        print(f'List size is {len(artists)}')
+        for i, j in zip(artists, titles):
+            _ += 1
+            print(f"Rank {str(_)} Artist : {i.find('a').text},  Title : {j.find('a').text}" )
 
 
+class Melon(object):
+    def __init__(self, url):
+        self.url = url
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
+
+    def scrap(self):
+        req = urllib.request.Request(self.url, headers= self.headers)
+        soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'lxml')
+        _ = 0
+        artists = soup.find_all(name='div', attrs={'class': 'ellipsis rank02'})
+        titles = soup.find_all(name='div', attrs={'class': 'ellipsis rank01'})
+        print(f'List size is {len(artists)}')
+        for i, j in zip(artists, titles):
+            _ += 1
+            print(f"Rank {str(_)} Artist : {i.find('a').text},  Title : {j.find('a').text}")
 
 def main():
     # 20210720
@@ -33,5 +48,6 @@ def main():
     Bugmusic(f'https://music.bugs.co.kr/chart/track/realtime/total?'
                   f'chartdate={"20210720"}&charthour={"16"}').scrap()
 
+
 if __name__ == '__main__':
-    main()
+    Melon('https://www.melon.com/chart/index.htm?dayTime=2021072016').scrap()
