46d196e65067fb7446c18878553883ccb9d3a7c3
parkjungkwan <pakjkwan@gmail.com>
2021-07-21 16:53:47 +0900

    16

--- scraping/music_ranking.py
+++ scraping/music_ranking.py
@@ -7,6 +7,8 @@ class MusicRanking(object):
     query_string = ''
     html = ''
     headers = {'User-Agent': 'Mozilla/5.0'}
+    tag_name = ''
+    fname = ''
     class_name = []
     artists = []
     titles = []
@@ -18,6 +20,27 @@ class MusicRanking(object):
         print(f'Crawling HTML is {self.html}')
     def get_raking(self):
         soup = BeautifulSoup(self.html, 'lxml')
+
+    def insert_dict(self):
+        # 방법 1
+        for i in range(0, len(self.tag_name)):
+            self.dict[self.titles[i]] = self.artists[i]
+        # 방법 2
+        for i, j in zip(self.titles, self.artists):
+            self.dict[i] = j
+        # 방법 3
+        for i, j in enumerate(self.titles):
+            self.dict[j] = self.artists[i]
+
+        print(dict)
+
+    def dict_to_dataframe(self):
+        self.df = pd.DataFrame.from_dict(self.dict, orient='index')
+        print(self.df)
+
+    def df_to_csv(self):
+        path = f'./data/{self.fname}.csv'
+        self.df.to_csv(path, sep=',', na_rep='NaN')
 
 def print_menu(ls):
     # return '\t'.join(ls)
@@ -41,10 +64,10 @@ def main():
             mr.domain = 'https://music.bugs.co.kr/chart/track/realtime/total?'
             mr.query_string = 'chartdate=20210721&charthour=09'
             mr.set_html()
-
         elif menu == 2:
-            mr.class_name.append('ellipsis rank02')
-            mr.class_name.append('ellipsis rank01')
+            mr.tag_name = 'p'
+            mr.class_name.append('artist')
+            mr.class_name.append('title')
             mr.get_raking()
         elif menu == 3:
             pass
